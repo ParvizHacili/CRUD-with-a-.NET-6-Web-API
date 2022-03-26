4ff7849f36cc0e17526dfca580fce6b7ade89d55
ParvizHacili <parviz.hajili@gmail.com>
2022-03-26 19:57:34 +0400

    Add project files.

---
+++ StudentAPI/Controllers/StudentController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public StudentController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> Get()
+        {
+            return Ok(await _context.Students.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> Get(int id)
+        {
+            var student =await  _context.Students.FindAsync(id);
+            if(student == null)
+            {
+                return BadRequest("Student Not Found");
+            }
+
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Student>>> AddStudent(Student student)
+        {
+             _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Students.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Student>>> UpdateStudent(Student request)
+        {
+            var student = await _context.Students.FindAsync(request.Id);
+            if (student == null)
+            {
+                return BadRequest("Student Not Found");
+            }
+
+            student.FirstName = request.FirstName;
+            student.LastName = request.LastName;
+            student.Place = request.Place;
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Students.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return BadRequest("Student Not Found");
+            }
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Students.ToListAsync());
+        }
+    }
+}
---
+++ StudentAPI/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentAPI
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+    }
+}
